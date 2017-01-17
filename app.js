cd4f85a6eb36b6f2ecaa9c2d8b50ee419253a5d4
xiaohang <xiaohang@nfmedia.com>
2017-01-17 19:33:17 +0800

    使用缓存保存用户信息，删除部分图片

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 //app.js
 App({
-  // data:{
-  //   // 配置初始化页面的图标
-  //   "icon_first_path":"images/2.png",
-  //   "select_icon_first_path":"images/1.png",
-  //   "icon_second_path":"images/3.png",
-  //   "select_icon_second_path":"images/4.png",
-  // },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
--- pages/aboutme/about.js
+++ pages/aboutme/about.js
@@ -1,5 +1,4 @@
 // pages/aboutme/about.js
-//index.js
 //获取应用实例
 var app = getApp()
 // console.log(app.globalData)
@@ -9,7 +8,7 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -17,11 +16,39 @@ Page({
   onLoad: function () {
     console.log('onLoad')
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+
+    // 如果本地存在用户信息，则不需要通过网络获取
+    wx.getStorage({
+      key: 'userinfo',
+      success: function (res) {
+        // success
+        console.log("从缓存中读取。")
+        that.setData({
+          userInfo: res.data
+        })
+      },
+      fail: function () {
+        console.log("缓存中没有数据，需要通过网络获取。。。")
+        that.getUserInfoAndSetStorage()
+      }
+    })
+  },
+  getUserInfoAndSetStorage: function () {
+    var that = this
+    console.log("getUserInfoAndSetStorage....")
+    app.getUserInfo(function (userInfo) {
+      // 将用户数据缓存在本地
+      wx.setStorage({
+        key: 'userinfo',
+        data: userInfo,
+        success: function (res) {
+          // success
+          console.log("缓存成功。")
+        }
+      })
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
